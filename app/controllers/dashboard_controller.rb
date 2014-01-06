39869123ca12baaff760a8394784ea949fdf2f68
Deactro <deactro@ws-159-97.han.nl>
2014-01-06 13:39:28 +0100

    Fixxed indexing messages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_filter :secure_page
   def index
     gflash :success => {:value => "The product has been created successfully!", :image => '',:sticky=> true}
+    index_messages;
 
     @user = current_user;
     @offers = Offer.within((current_user.radius.to_f / 1000).to_f, :origin => @user);
