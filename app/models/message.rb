432ea5f80e3b2ffa85402d51be4b906574782ff9
Natnael Y. Abbai <natnaelyntn@gmail.com>
2014-01-09 14:01:48 +0100

    groupby fixx

--- app/models/message.rb
+++ app/models/message.rb
@@ -14,7 +14,7 @@ class Message < ActiveRecord::Base
     Message.where(from_user: [from_user, to_user], to_user: [from_user, to_user], message_type: 'chat');
   end
   def self.getwebmessages
-    messages = Message.all.where(:message_type => 'web').group(:from_user).having(:to_user=='')
+    messages = Message.all.where(:message_type => 'web').group(:from_user, :id,:to_user, :body, :message_type, :reaction_to)
     messages
   end
 
