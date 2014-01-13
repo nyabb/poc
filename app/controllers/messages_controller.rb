3a75edf3e2be5458fb5af6bb59c5649164bac534
Natnael Y. Abbai <natnaelyntn@gmail.com>
2014-01-13 23:14:26 +0100

    no message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    index_messages.paginate(:page => params[:page], :per_page => 2)
+    index_messagess
     @current_user = current_user
     @messages = Message.getwebmessages
     @messages = @messages.paginate(:page => params[:page], :per_page => 2)
